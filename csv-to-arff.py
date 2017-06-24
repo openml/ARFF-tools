c13b7092ca20934221ebb78d1ed6c45fc01d8099
Pieter Gijsbers <p.gijsbers@student.tue.nl>
2017-06-24 12:24:11 +0200

    More refactoring, script now also recognizes string as attribute type.

--- csv-to-arff.py
+++ csv-to-arff.py
@@ -1,6 +1,8 @@
 """
 This script converts csv datasets to ARFF format.
 In the ARFF format, one needs to know whether features are numeric or categorical - this is done with a simple heuristic, and thus is not always completely accurate.
+No support for Data attributes yet.
+Categorical attributes should be encoded as numbers
 """
 import sys
 import math
@@ -27,22 +29,27 @@ column_info = []
 integer_columns = []
 for column in dataset.columns:
 	column_values = dataset[column]
-	row_is_integer = list_only_int(column_values)
-	integer_columns.append(row_is_integer)
-	not_nan_values = column_values[~numpy.isnan(column_values)]
-	unique_values = numpy.unique(not_nan_values)
-	is_categorical = (len(unique_values) <= 10 and row_is_integer)
-	if is_categorical:
-		data_type_str = '{{{}}}'.format(','.join([str(int(val)) for val in unique_values]))
+	column_is_integer = list_only_int(column_values)
+	integer_columns.append(column_is_integer)
+
+	if column_is_integer:
+		not_nan_values = column_values[~numpy.isnan(column_values)]
+		unique_values = numpy.unique(not_nan_values)
+		is_categorical = (len(unique_values) <= 10 and column_is_integer)
+		if is_categorical:
+			data_type_str = '{{{}}}'.format(','.join([str(int(val)) for val in unique_values]))
+		else:
+			data_type_str = 'NUMERIC'
 	else:
-		data_type_str = 'NUMERIC'
+		data_type_str = 'string' if all([isinstance(val, str) for val in column_values]) else 'NUMERIC'
+
 	column_info.append((column, data_type_str))
 
 with open('{}.arff'.format(dataset_name), 'w') as arff_file:
 	arff_file.write('% This ARFF is automatically generated.\n'
 			'% The data types of attributes are inferred from the data.\n'
 			'% In particular this means there is some uncertainty as to whether '
-			'an attribute truly is categorical, numerical or ordinal.\n'
+			'an attribute truly is categorical or numerical.\n'
 			'\n'
 			'@RELATION {}\n'.format(dataset_name))
 	for (name, data_type) in column_info:
@@ -50,10 +57,10 @@ with open('{}.arff'.format(dataset_name), 'w') as arff_file:
 
 	arff_file.write('@DATA\n')
 	for row in dataset.as_matrix():
-		row_str_els = []
-		for column, el in enumerate(row):
-			if not math.isnan(el) and integer_columns[column]:
-				el = int(el)
-			row_str_els.append(str(el))
-		row_str = ','.join([el for el in row_str_els])
-		arff_file.write('{}\n'.format(row_str))
+		row_values = [str(int(val)) if integer_columns[column] and 
+					       not math.isnan(val)
+				            else str(val)
+				            for (column, val) in enumerate(row)]
+
+		row_str = ','.join(row_values)
+		arff_file.write(row_str+'\n')
