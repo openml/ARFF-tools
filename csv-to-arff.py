dacc3fe3df7619d12d0087fa02ff666664356258
Pieter Gijsbers <p.gijsbers@student.tue.nl>
2017-06-24 11:41:11 +0200

    Refactored list_only_int and renamed script.

--- csv-to-arff.py
+++ csv-to-arff.py
@@ -1,30 +1,27 @@
 """
-This script converts the datasets of PMLB to ARFF format.
+This script converts csv datasets to ARFF format.
 In the ARFF format, one needs to know whether features are numeric or categorical - this is done with a simple heuristic, and thus is not always completely accurate.
 """
 import sys
 import math
 import numpy
-from pmlb import fetch_data
-pmlb_cache_dir = '../experiments/data/'
+import pandas as pd
+import numbers
 
 def list_only_int(lst):
-	row_is_integer = True
-	for el in lst:
-		is_int = False
-		try:
-			if math.isnan(el):
-				is_int = True
-			else:
-				is_int = float(el).is_integer()
-		except:
-			pass
-		finally:
-			row_is_integer &= is_int
-	return row_is_integer
+	return (all([isinstance(el, numbers.Number) for el in lst]) and
+		all([math.isnan(el) or float(el).is_integer() for el in lst]))
 
-dataset_name = sys.argv[1]
-dataset = fetch_data(dataset_name, local_cache_dir=pmlb_cache_dir)
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+if '/' in input_file:
+	end_of_folder_path = input_file.rfind('/') + 1
+else:
+	end_of_folder_path = 0
+
+dataset_name = input_file[end_of_folder_path:-4]
+dataset = pd.read_csv(input_file, sep='\t')
 
 column_info = []
 integer_columns = []
@@ -41,9 +38,8 @@ for column in dataset.columns:
 		data_type_str = 'NUMERIC'
 	column_info.append((column, data_type_str))
 
-with open('output_folder/{}.arff'.format(dataset_name), 'w') as arff_file:
-	arff_file.write('% This dataset is taken from the Penn Machine Learning Benchmark.\n'
-			'% This ARFF is automatically generated.\n'
+with open('{}.arff'.format(dataset_name), 'w') as arff_file:
+	arff_file.write('% This ARFF is automatically generated.\n'
 			'% The data types of attributes are inferred from the data.\n'
 			'% In particular this means there is some uncertainty as to whether '
 			'an attribute truly is categorical, numerical or ordinal.\n'
